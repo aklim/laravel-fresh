27b66076e44f3379dc8f200545d5e18270b47db7
andreyklimenko <andrey.iemail@gmail.com>
2019-12-09 13:00:58 +0200

    Add user authentication tests

---
+++ tests/Browser/ExampleTest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Tests\Browser;
+
+use App\Eloquent\User;
+use Tests\DuskTestCase;
+use Laravel\Dusk\Browser;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+
+class ExampleTest extends DuskTestCase
+{
+    use DatabaseMigrations;
+
+    /**
+     * A basic browser test example.
+     *
+     * @return void
+     * @throws \Throwable
+     */
+    public function testBasicExample()
+    {
+        $user = factory(User::class)->create([
+            'email' => 'taylor@laravel.com',
+        ]);
+
+        $this->browse(function (Browser $browser) use ($user) {
+            $browser->visit('/login')
+                ->type('email', $user->email)
+                ->type('password', 'password')
+                ->press('Login')
+                ->assertPathIs('/home');
+        });
+
+    }
+}
